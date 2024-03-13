386578b4174ab5c92493f6482dbf1dc67bdcf516
thrombe <yash.n.thombare620@gmail.com>
2024-03-13 14:17:37 +0530

    fixed pipe movement

--- Assets/pipe_script.cs
+++ Assets/pipe_script.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class pipe_script : MonoBehaviour
 {
-    public float move_speed = 0.02f;
+    float move_speed = 0.4f;
     float death_x = -2.5f;
 
     // Start is called before the first frame update
@@ -16,7 +16,7 @@ public class pipe_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.left * move_speed;
+        transform.position += Vector3.left * move_speed * Time.deltaTime;
 
 
         if (transform.position.x < death_x) {
